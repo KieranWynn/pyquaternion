6a9adc8f6e21ace2f3e3452a68b6cce09a53ae20
Kieran Wynn <kieranwynn+git@gmail.com>
2016-03-13 13:11:38 +1000

    Deployed f4f70a2 with MkDocs version: 0.15.3

--- js/base.js
+++ js/base.js
@@ -1,19 +1,48 @@
+/* Search */
+
+function getSearchTerm()
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == 'q') {
+            return sParameterName[1];
+        }
+    }
+}
+
+$(document).ready(function() {
+    var search_term = getSearchTerm(),
+        $search_modal = $('#mkdocs_search_modal');
+
+    if(search_term) {
+        $search_modal.modal();
+    }
+
+    $search_modal.on('shown.bs.modal', function () {
+        $search_modal.find('#mkdocs-search-query').focus();
+    });
+});
+
 
 /* Highlight */
 $( document ).ready(function() {
     hljs.initHighlightingOnLoad();
-    $('table').addClass('table');
+    $('table').addClass('table table-striped table-hover');
 });
 
 
-/* Scrollspy */
-var navHeight = $('.navbar').outerHeight(true) + 10
-
 $('body').scrollspy({
     target: '.bs-sidebar',
-    offset: navHeight
-})
+});
 
+/* Toggle the `clicky` class on the body when clicking links to let us
+   retrigger CSS animations. See ../css/base.css for more details. */
+$('a').click(function(e) {
+    $('body').toggleClass('clicky');
+});
 
 /* Prevent disabled links from causing a page reload */
 $("li.disabled a").click(function() {
@@ -21,33 +50,4 @@ $("li.disabled a").click(function() {
 });
 
 
-/* Adjust the scroll height of anchors to compensate for the fixed navbar */
-window.disableShift = false;
-var shiftWindow = function() {
-    if (window.disableShift) {
-        window.disableShift = false;
-    } else {
-        /* If we're at the bottom of the page, don't erronously scroll up */
-        var scrolledToBottomOfPage = (
-            (window.innerHeight + window.scrollY) >= document.body.offsetHeight
-        );
-        if (!scrolledToBottomOfPage) {
-            scrollBy(0, -60);
-        };
-    };
-};
-if (location.hash) {shiftWindow();}
-window.addEventListener("hashchange", shiftWindow);
 
-
-/* Deal with clicks on nav links that do not change the current anchor link. */
-$("ul.nav a" ).click(function() {
-    var href = this.href;
-    var suffix = location.hash;
-    var matchesCurrentHash = (href.indexOf(suffix, href.length - suffix.length) !== -1);
-    if (location.hash && matchesCurrentHash) {
-        /* Force a single 'hashchange' event to occur after the click event */
-        window.disableShift = true;
-        location.hash='';
-    };
-});

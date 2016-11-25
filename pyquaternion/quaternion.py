fix wrong order of rotations

pyquaternion/quaternion.py
@@ -737,16 +737,18 @@ class Quaternion:
             yaw:    rotation angle around the z-axis in radians, in the range `[-pi, pi]`
             pitch:  rotation angle around the y'-axis in radians, in the range `[-pi/2, -pi/2]`
             roll:   rotation angle around the x''-axis in radians, in the range `[-pi, pi]` 
+        
+        The resulting rotation_matrix would be R = R_x(roll) R_y(pitch) R_z(yaw)
             
         Note: 
             This feature only makes sense when referring to a unit quaternion. Calling this method will implicitly normalise the Quaternion object to a unit quaternion if it is not already one.
         """
         
         self._normalise()
-        yaw = np.arctan2(2*(self.q[0]*self.q[3] + self.q[1]*self.q[2]), 
+        yaw = np.arctan2(2*(self.q[0]*self.q[3] - self.q[1]*self.q[2]), 
             1 - 2*(self.q[2]**2 + self.q[3]**2))
-        pitch = np.arcsin(2*(self.q[0]*self.q[2] - self.q[3]*self.q[1]))
-        roll = np.arctan2(2*(self.q[0]*self.q[1] + self.q[2]*self.q[3]), 
+        pitch = np.arcsin(2*(self.q[0]*self.q[2] + self.q[3]*self.q[1]))
+        roll = np.arctan2(2*(self.q[0]*self.q[1] - self.q[2]*self.q[3]), 
             1 - 2*(self.q[1]**2 + self.q[2]**2))
 
         return yaw, pitch, roll         

pyquaternion/test/test_quaternion.py
@@ -821,13 +821,19 @@ class TestQuaternionFeatures(unittest.TestCase):
                 [ s, c, 0],
                 [ 0, 0, 1]])
 
+        p = np.random.randn(3)
         q = Quaternion.random()
         yaw, pitch, roll = q.yaw_pitch_roll
 
+        p_q = q.rotate(p)
+        R_q = q.rotation_matrix
+
         # build rotation matrix, R = R_z(yaw)*R_y(pitch)*R_x(roll)
-        R = np.dot(np.dot(R_z(yaw), R_y(pitch)), R_x(roll))
+        R_ypr = np.dot(R_x(roll), np.dot(R_y(pitch), R_z(yaw)))
+        p_ypr = np.dot(R_ypr, p)
         
-        np.testing.assert_almost_equal(R, q.rotation_matrix, decimal=ALMOST_EQUAL_TOLERANCE)
+        np.testing.assert_almost_equal(p_q , p_ypr, decimal=ALMOST_EQUAL_TOLERANCE)
+        np.testing.assert_almost_equal(R_q , R_ypr, decimal=ALMOST_EQUAL_TOLERANCE)
 
     def test_matrix_io(self):
         v = np.random.uniform(-100, 100, 3)
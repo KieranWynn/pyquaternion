aac0af5aa1b590aaf24d5949f3e2a1e09adc291e
Kieran Wynn <kieran.wynn@polymathian.com>
2019-11-10 19:46:11 +1100

    Update deploy config

--- setup.py
+++ setup.py
@@ -92,6 +92,8 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
+        'dev': ["mkdocs"],
+        'test': ["nose"]
     },
 
     # If there are data files included in your packages that need to be
